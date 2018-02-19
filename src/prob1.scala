5cc4294d9371fb7be393f173b43befc4a9b32f90
jsburke <jsburke@bu.edu>
2018-02-18 22:48:46 -0500

    add comment

--- src/prob1.scala
+++ src/prob1.scala
@@ -2,6 +2,8 @@
 import scala.util.Random
 import scala.collection.immutable.List._
 
+// get last item in list
+
 object Main extends App {
   val xs = (0 to Random.nextInt(42) + 1).toList 
   xs map {n => print(n + "  ")}
