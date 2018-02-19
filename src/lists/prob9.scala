afbd4494d1abc91b17aeeb401696ba78e52eb935
jsburke <jsburke@bu.edu>
2018-02-19 10:25:16 -0500

    start hacking p9

---
+++ src/lists/prob9.scala
@@ -0,0 +1,11 @@
+
+object prob9 {
+  def pack[T](xs: List[T]): List[List[T]] = {
+    def packGuts[T](ys: List[T], ps: List[List[T]], prev: Option[T]): List[List[T]] = {
+      prev match {
+        Some[T] => 
+      }
+    }
+    packGuts(xs, List(List()), None)
+  }
+}
